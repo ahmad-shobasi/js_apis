e4cbe88889193a4b4369ea9d73ded1ca3668207d
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-02-04 14:11:34 +0300

    enable cors and use global pips.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // configure swagger UI.
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
@@ -12,7 +14,21 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+  //
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: ['https://frontend.com', 'https://another-frontend.com'], // Allowed origins
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
