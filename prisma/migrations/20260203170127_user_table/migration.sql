bde633d111ce639fdf86e8699927767ce3b7bbb4
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-02-04 14:09:34 +0300

    Create user table schema.

---
+++ prisma/migrations/20260203170127_user_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'USER', 'GUEST');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
