ba77ea61352da3aae304d3fb85671d31f42d6951
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-02-05 12:30:25 +0300

    import auth module in the app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { EmployeesModule } from './employees/employees.module';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     UsersModule,
@@ -30,6 +31,8 @@ import { APP_GUARD } from '@nestjs/core';
         limit: 100,
       },
     ]),
+
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
