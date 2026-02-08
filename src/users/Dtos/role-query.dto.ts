03677c7710555f4b8fef9d78dc48e859fcf2033d
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-02-08 16:03:56 +0300

    remove unused user module (now the app interacting with users table throw auth module).

--- src/users/Dtos/role-query.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UserRole } from '@prisma/client';
-import { IsEnum, IsOptional } from 'class-validator';
-
-export class GetUserQueryDto {
-  @IsOptional()
-  @IsEnum(UserRole, { message: 'role must be valid' })
-  role?: UserRole;
-}
