8c930684057afe56a0f6cb5e7b7e8dd20d6a0175
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-02-08 16:01:11 +0300

    Improve the database connection with prisma client.

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -2,14 +2,18 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { env } from 'prisma/config';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
   constructor() {
     const pool = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+      connectionString: env('DATABASE_URL'),
     });
-    super({ adapter: pool });
+    super({
+      adapter: pool,
+    });
   }
   async onModuleInit() {
     await this.$connect();
