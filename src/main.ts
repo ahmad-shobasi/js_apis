0ff76323035249e462c878ae85747c269f08b367
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-02-25 15:42:55 +0300

    Install bullmq board packages and enable bull UI dashboard.

--- src/main.ts
+++ src/main.ts
@@ -9,31 +9,43 @@ import { HttpLoggingInterceptor } from './common/logger/http-logging.interceptor
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ExpressAdapter } from '@bull-board/express';
+import { Queue } from 'bullmq';
+import { getQueueToken } from '@nestjs/bullmq';
+import { MAIL_QUEUE } from './mail/mail.constant';
+import { createBullBoard } from '@bull-board/api';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bufferLogs: true,
   });
   const appConfig = app.get(ConfigService);
-  // Use static folder path for uploaded images
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads/',
+
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath('/admin/queues');
+
+  // Get queue instance from NestJS's dependency injection container.
+  const mailQueue = app.get<Queue>(getQueueToken(MAIL_QUEUE));
+
+  // Set up Bull Board with the queue and server adapter.
+  createBullBoard({
+    queues: [new BullMQAdapter(mailQueue)],
+    serverAdapter,
   });
+
   // configure swagger UI.
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
-  app.use(
-    '/reference',
-    apiReference({
-      content: documentFactory,
-    }),
-  );
-  //
+  // Use static folder path for uploaded images
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: ['https://frontend.com', 'https://another-frontend.com'], // Allowed origins
@@ -41,6 +53,21 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
+
+  // NOTE: Only enable API docs and Bull Board in non-production environments
+  if (process.env.NODE_ENV !== 'production') {
+    app.use('/admin/queues', serverAdapter.getRouter());
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api', app, documentFactory);
+
+    app.use(
+      '/reference',
+      apiReference({
+        content: documentFactory,
+      }),
+    );
+  }
 
   app.useGlobalPipes(
     new ValidationPipe({
