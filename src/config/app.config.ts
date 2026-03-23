a4a9fee17529f5438af5f000a54516ffdde88daa
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-03-23 13:51:39 +0300

    Add nodemailer package and setup smtp config values in environments.

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -17,4 +17,15 @@ export default () => ({
     host: process.env.REDIS_HOST,
     port: parseInt(process.env.REDIS_PORT || '6379', 10),
   },
+
+  mail: {
+    smtp: {
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT || '587', 10),
+      secure: process.env.SMTP_SECURE === 'true',
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+    from: process.env.MAIL_FROM,
+  },
 });
